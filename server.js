872ad6435edc93c8ffdba5e1191769e933859067
Eric Muyser <eric@muyser.com>
2016-05-19 08:48:21 -0700

    Refactor game server

--- server.js
+++ server.js
@@ -4,158 +4,170 @@ var http = require('http');
 var app = express();
 
 app.use(express.static(__dirname));
+
 // app.configure(function() {
 //     app.use(express.static(__dirname));
 //     app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
 // });
 
-var server = http.createServer(app);
-var io = require('socket.io').listen(server);
-var clients = [];
-var host = null;
-
-
 function getRandomInt(min, max) {
     return Math.floor(Math.random() * (max - min + 1) + min);
 }
 
-var setHost = function(client) {
-    console.log('Setting host to player: ' + client.player.id);
-    host = client;
-};
-
-var findNewHost = function() {
-    if (clients.length > 0) {
-        var i = getRandomInt(0, clients.length-1);
-        var client = clients[i];
-
-        // Make sure client had time to initialize the player
-        if (client) {
-            setHost(client);
-
-            console.log('New host: ' + host.player.id);
-            io.sockets.emit('setHost', {player: host.player});
-        }
-    }
-};
-
-var getClientHost = function() {
-    if (!clients.length) { return; }
-    return clients.reduce(function(previousClient, currentClient) { if (previousClient && previousClient.player.id === host.player.id) { return previousClient; } else if (currentClient.player.id === host.player.id) { return currentClient; }});
-};
-
-var findClientBySocket = function(socket) {
-    if (!clients.length) { return; }
-    return clients.reduce(function(previousClient, currentClient) { if (previousClient && previousClient.socket === socket) { return previousClient; } else if (currentClient.socket === socket) { return currentClient; }});
-};
-
-var addClient = function(client) {
-    console.log('Adding player: ' + client.player.id);
-    clients.push(client);
-};
-
-var removeClient = function(client) {
-    console.log('Removing player: ' + client.player.id);
-
-    clients.splice(clients.indexOf(client), 1);
-};
-
-// Monitor the clients to make sure they are still defined
-var monitorHost = function() {
-    if (!host) {
-        findNewHost();
-    }
-
-    setTimeout(monitorHost, 50);
-};
-
-var Hackatron = function() {
+var Game = function() {
+    this.server = http.createServer(app);
+    this.io = require('socket.io').listen(this.server);
     this.events = [];
+    this.clients = [];
+    this.host = null;
 };
 
-Hackatron.prototype = {
+Game.prototype = {
+    startServer: function() {
+        this.io.sockets.on('connection', function(socket) {
+            this.onSocketConnect(socket);
+
+            socket.on('disconnect', function() {
+                this.onSocketDisconnect(socket);
+            }.bind(this));
+        }.bind(this));
+
+        console.log('Open localhost:8080 on your browser.');
+        console.log('Listening...');
+
+        this.monitorHost();
+
+        setInterval(function() {
+            if (!this.host) return console.log('No host');
+            if (!this.host) return console.log('Host has no player');
+
+            console.log('Host: ', this.host.player.id);
+        }.bind(this), 2000);
+
+        this.server.listen(process.env.PORT || 8080);
+    },
+
+    setHost: function(client) {
+        console.log('Setting host to player: ' + client.player.id);
+        this.host = client;
+    },
+
+    findNewHost: function() {
+        if (this.clients.length > 0) {
+            var i = getRandomInt(0, this.clients.length-1);
+            var client = this.clients[i];
+
+            // Make sure client had time to initialize the player
+            if (client) {
+                this.setHost(client);
+
+                console.log('New host: ' + this.host.player.id);
+                this.io.sockets.emit('setHost', {player: this.host.player});
+            }
+        }
+    },
+
+    getClientHost: function() {
+        if (!this.clients.length) { return; }
+        return this.clients.reduce(function(previousClient, currentClient) { if (previousClient && previousClient.player.id === this.host.player.id) { return previousClient; } else if (currentClient.player.id === this.host.player.id) { return currentClient; }});
+    },
+
+    findClientBySocket: function(socket) {
+        if (!this.clients.length) { return; }
+        return this.clients.reduce(function(previousClient, currentClient) { if (previousClient && previousClient.socket === socket) { return previousClient; } else if (currentClient.socket === socket) { return currentClient; }});
+    },
+
+    addClient: function(client) {
+        console.log('Adding player: ' + client.player.id);
+        this.clients.push(client);
+    },
+
+    removeClient: function(client) {
+        console.log('Removing player: ' + client.player.id);
+
+        this.clients.splice(this.clients.indexOf(client), 1);
+    },
+
     fireEvent: function(socket, event) {
         socket.emit('events', {events: [event]});
     },
+
     fireAllPlayers: function(event) {
-        io.sockets.emit('events', {events: [event]});
-    }
-};
+        this.io.sockets.emit('events', {events: [event]});
+    },
 
-var hackatron = new Hackatron();
+    parseEvent: function(socket, event) {
+        if (event.key === 'newPlayer') {
+            console.log('Handshaking...');
 
-var parseEvent = function(socket, event) {
-    if (event.key === 'newPlayer') {
-        console.log('Handshaking...');
+            this.addClient({socket: socket, player: event.info.player});
 
-        addClient({socket: socket, player: event.info.player});
+            // If it's the first client or there's no hosts, lets set it as the new host
+            if (!this.host) {
+                this.setHost(this.clients[this.clients.length-1]);
+                console.log('New host: ' + this.host.player.id);
+            }
 
-        // If it's the first client or there's no hosts, lets set it as the new host
-        if (!host) {
-            setHost(clients[clients.length-1]);
-            console.log('New host: ' + host.player.id);
+            this.fireEvent(socket, {key: 'setHost', info: {player: this.host.player}});
+        } else if (event.key === 'this.findNewHost') {
+            console.log("Finding new host....");
+            var client = this.findClientBySocket(socket);
+            this.removeClient(client);
+            this.host = null;
+            this.findNewHost();
+        } else {
+            //socket.broadcast.emit(event.key, event.info);
         }
+    },
 
-        hackatron.fireEvent(socket, {key: 'setHost', info: {player: host.player}});
-    } else if (event.key === 'findNewHost') {
-        console.log("Finding new host....");
-        var client = findClientBySocket(socket);
-        removeClient(client);
-        host = null;
-        findNewHost();
-    } else {
-        //socket.broadcast.emit(event.key, event.info);
-    }
-};
+    onSocketConnect: function(socket) {
+        console.log('New connection.. waiting for handshake');
 
-io.sockets.on('connection', function(socket) {
-    console.log('New connection.. waiting for handshake');
+        // TODO: give them 10 seconds to identify as a newPlayer, or cut them off
 
-    // TODO: give them 10 seconds to identify as a newPlayer, or cut them off
+        socket.on('events', function(data) {
+            //console.log('Incoming events: ' + data);
+            data = JSON.parse(data);
 
-    socket.on('events', function(data) {
-        //console.log('Incoming events: ' + data);
-        data = JSON.parse(data);
+            data.events.forEach(function(event) { this.parseEvent(socket, event); }.bind(this));
 
-        data.events.forEach(function(event) { parseEvent(socket, event); });
+            socket.broadcast.emit('events', data);
+        }.bind(this));
+    },
 
-        socket.broadcast.emit('events', data);
-    });
-
-    socket.on('disconnect', function() {
-        var client = findClientBySocket(socket);
+    onSocketDisconnect: function(socket) {
+        var client = this.findClientBySocket(socket);
 
         if (!client) { return; }
 
-        removeClient(client);
+        this.removeClient(client);
 
         // If this client was the host,
         // and there's at least one more client connected,
         // lets choose a new random host,
         // and broadcast it to everybody
-        if (client.player.id === host.player.id) {
-            host = null;
-            findNewHost();
+        if (client.player.id === this.host.player.id) {
+            this.host = null;
+            this.findNewHost();
         }
 
-        if (clients.length === 0) {
-            host = null;
+        if (this.clients.length === 0) {
+            this.host = null;
         }
 
-        hackatron.fireAllPlayers({key: 'playerLeave', info: {player: client.player}});
-    });
-});
+        this.fireAllPlayers({key: 'playerLeave', info: {player: client.player}});
+    },
 
-monitorHost();
+    // Monitor the clients to make sure they are still defined
+    monitorHost: function() {
+        if (!this.host) {
+            this.findNewHost();
+        }
 
-console.log('Open localhost:8080 on your browser.');
-console.log('Listening...');
+        setTimeout(this.monitorHost.bind(this), 50);
+    }
+};
 
-setInterval(function() {
-    if (!host) return console.log('No host');
-    if (!host) return console.log('Host has no player');
+var server = new Game();
 
-    console.log('Host: ', host.player.id);
-}, 2000);
-
-server.listen(process.env.PORT || 8080);
+server.startServer();
